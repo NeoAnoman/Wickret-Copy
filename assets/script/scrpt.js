c1c77e09b6caaddfaef223422095b7048e8e6600
Neo Anoman <neoanoman@gmail.com>
2020-06-19 13:12:27 +0530

    moreChanges

--- assets/script/scrpt.js
+++ assets/script/scrpt.js
@@ -2,3 +2,20 @@ function btnClk(prop) {
   document.querySelector('.slct').classList.remove('slct');
   document.querySelector('#'+prop).classList.add('slct');
 }
+function showAll() {
+  document.querySelectorAll('.hideMe').forEach((item, i) => {
+    item.classList.remove('hideMe')
+  });
+  document.querySelectorAll('.moveup').forEach((item, i) => {
+    item.classList.remove('moveup')
+  });
+  document.querySelectorAll('.moveup2').forEach((item, i) => {
+    item.classList.remove('moveup2')
+  });
+  document.querySelectorAll('.scaleMe').forEach((item, i) => {
+    item.classList.remove('scaleMe')
+  });
+  document.querySelectorAll('.moveup3').forEach((item, i) => {
+    item.classList.remove('moveup3')
+  });
+}
