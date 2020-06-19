5f2bfc6785f0d849da10e137ac68c2e38a9f9ab4
Neo Anoman <neoanoman@gmail.com>
2020-06-19 16:52:25 +0530

    scroll added

--- assets/script/scrpt.js
+++ assets/script/scrpt.js
@@ -6,8 +6,8 @@ function showAll() {
   document.querySelectorAll('.hideMe').forEach((item, i) => {
     item.classList.remove('hideMe')
   });
-  document.querySelectorAll('.moveup').forEach((item, i) => {
-    item.classList.remove('moveup')
+  document.querySelectorAll('.moveup1').forEach((item, i) => {
+    item.classList.remove('moveup1')
   });
   document.querySelectorAll('.moveup2').forEach((item, i) => {
     item.classList.remove('moveup2')
@@ -18,4 +18,32 @@ function showAll() {
   document.querySelectorAll('.moveup3').forEach((item, i) => {
     item.classList.remove('moveup3')
   });
+}
+window.onscroll = function() {
+  var scroll = window.pageYOffset;
+  var Y = 100;
+  if (scroll > Y){
+    showAll();
+  }
+  else {
+    if(document.querySelectorAll('.hideMe').length==0) {
+      hideAll();
+    }
+  }
+}
+function hideAll() {
+  var i=1;
+  document.querySelectorAll('.trns').forEach((item, i) => {
+    if(item.classList.contains("mvp")) {
+      item.classList.add("moveup"+i);
+      i++;
+    }
+    else if (item.classList.contains("scl")) {
+      item.classList.add("scaleMe");
+    }
+    else {
+      item.classList.add("hideMe");
+    }
+  });
+
 }
